e9431cc56e243a33e63486c4a5958f86847b64ba
DianaSanchezOrdonez <dianaordonez1998@gmail.com>
2022-09-18 00:50:08 -0500

    feat: add home page with css

---
+++ src/Home.tsx
@@ -0,0 +1,193 @@
+import { useEffect, useState } from 'react'
+import { Link, Router } from 'react-router-dom'
+import './home.css'
+import reactImage from './assets/react.png'
+import angularImage from './assets/angular.png'
+import vueImage from './assets/vue.png'
+import clockImage from './assets/clock.svg'
+import favoriteImage from './assets/favorite.svg'
+
+interface IHits {
+  readonly author: string
+  readonly comment_text: string
+  readonly created_at: '2022-09-17T09:19:27.000Z'
+  readonly created_at_i: 1663406367
+  readonly num_comments?: string
+  readonly objectID: '32876047'
+  readonly parent_id: 32874213
+  readonly points?: string
+  readonly story_id: 32874086
+  readonly story_text?: string
+  readonly story_title: string
+  readonly story_url: string
+  readonly title?: string
+  readonly url?: string
+}
+
+function Home() {
+  const [data, setData] = useState<IHits[]>([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch('https://hn.algolia.com/api/v1/search_by_date?query=reactjs&page=0')
+      const { hits } = await res.json()
+      console.log('hits', hits)
+      setData(hits)
+    }
+
+    fetchData()
+  }, [])
+
+  const optionSelected = () => {
+    console.log('hello...')
+    const selectText = document.querySelector('.select-text')
+    const options: Element[] = Array.from(document.getElementsByClassName('options'))
+    const list = document.querySelector('.list')
+    const arrowIcon = document.getElementById('arrow-icon')
+
+    list?.classList.toggle('hide')
+    arrowIcon?.classList.toggle('rotate')
+
+    options.forEach((option) => {
+      option.addEventListener('click', (e) => {
+        console.log('👁 ', e)
+        // list?.classList.toggle('hide')
+        // arrowIcon?.classList.toggle('rotate')
+      })
+    })
+  }
+
+  return (
+    <div className='container'>
+      <header className='header'>
+        <h1>hacker news</h1>
+      </header>
+
+      <div className='filter'>
+        <Link to='/' className='selected'>
+          All
+        </Link>
+        <Link to='/favorites'>My faves</Link>
+      </div>
+
+      <div className='selector' onClick={optionSelected}>
+        <div>
+          <p className='select-text'>Select your news</p>
+          <i className='fa fa-chevron-down' id='arrow-icon'></i>
+        </div>
+
+        <ul className='list hide'>
+          <li className='options'>
+            <img src={angularImage} alt='angular' />
+            <p>Angular</p>
+          </li>
+          <li className='options'>
+            <img src={reactImage} alt='reactjs' />
+            <p>Reactjs</p>
+          </li>
+          <li className='options'>
+            <img src={vueImage} alt='reactjs' />
+            <p>Vuejs</p>
+          </li>
+        </ul>
+      </div>
+
+      <div className='grid-cards'>
+        {data.map((hit, index) => {
+          return (
+            <div className='grid-cards-inner' key={index}>
+              <div className='card-body'>
+                <div>
+                  <img src={clockImage} alt='clock' />
+                  <span>3 hours ago by {hit.author}</span>
+                </div>
+                <p>{hit.story_title}</p>
+              </div>
+              <div className='action-fav'>
+                <input type='image' src={favoriteImage} alt='submit'></input>
+              </div>
+            </div>
+          )
+        })}
+        {/* {data.map((el) => {
+          ;<div className='grid-cards-inner'>
+            <div className='card-body'>
+              <div>
+                <img src={clockImage} alt='clock' />
+                <span>3 hours ago by author</span>
+              </div>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat distinctio dolor, nisi laborum quos
+                odio unde consectetur asperiores odit omnis!
+              </p>
+            </div>
+            <div className='action-fav'>
+              <input type='image' src={favoriteImage} alt='submit'></input>
+            </div>
+          </div>
+        })} */}
+      </div>
+      {/* <main className='content'>
+        <div className='filter'>
+          <button className='all'>All</button>
+          <button className='favorites'>My faves</button>
+        </div>
+        <div className='filter-stack'>
+          <select>
+            <option>Select your news</option>
+            <option>Angular</option>
+            <option>
+              <img src={reactImage} alt='reactjs' />
+              Reactjs
+            </option>
+            <option>
+              <img src={reactImage} alt='reactjs' />
+              Vuejs
+            </option>
+          </select>
+        </div>
+      </main> */}
+      {/* <div className='cards-content'>
+        <div className='card'>
+          <div className='card-body'>
+            <div>
+              <img src={clockImage} alt='clock' />
+              <p>3 hours ago by author</p>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, assumenda.</p>
+          </div>
+          <div className='card-action'>
+            <img src={favoriteImage} alt='heart' />
+          </div>
+        </div>
+        <div className='card'>
+          <div className='card-body'>
+            <div>
+              <img src={clockImage} alt='clock' />
+              <p>3 hours ago by author</p>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, assumenda.</p>
+          </div>
+          <div className='card-action'>
+            <img src={favoriteImage} alt='heart' />
+          </div>
+        </div>
+        <div className='card'>
+          <div className='card-body'>
+            <div>
+              <img src={clockImage} alt='clock' />
+              <p>3 hours ago by author</p>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, assumenda.</p>
+          </div>
+          <div className='card-action'>
+            <img src={favoriteImage} alt='heart' />
+          </div>
+        </div>
+      </div> */}
+      <div className='pagination'></div>
+    </div>
+  )
+}
+
+export default Home
