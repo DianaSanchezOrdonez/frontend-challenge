40de552460b4afac2eb4b1052c30b79de983f437
DianaSanchezOrdonez <dianaordonez1998@gmail.com>
2022-09-18 23:38:47 -0500

    chore: remove comments to fetchAllHits hook

--- src/helpers/fetchAllHits.tsx
+++ src/helpers/fetchAllHits.tsx
@@ -1,13 +1,9 @@
-import { FetchAllHitsResponse, Hit } from '../interfaces/fetchAllHitsResponse'
+import { FetchAllHitsResponse } from '../interfaces/fetchAllHitsResponse'
 
 const baseUrl = 'https://hn.algolia.com/api/v1'
 
 export const fetchAllHits = async (technology: string, page: number): Promise<FetchAllHitsResponse> => {
   const res = await fetch(`${baseUrl}/search_by_date?query=${technology}&page=${page}`)
-  // console.log('testing', await res.json())
-  // hitsPerPage: 20
-  // nbHits: 5867
-  // nbPages: 50
-  // page: 0
+
   return res.json()
 }
